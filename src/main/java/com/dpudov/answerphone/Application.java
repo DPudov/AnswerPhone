3ddd6d2ca257ca1835ba58befc26366cd93fb5ca
DPudov <dimapudov99@gmail.com>
2016-01-31 19:19:50 +0300

    Added too much features, fragments, drawer, VKSdk

---
+++ src/main/java/com/dpudov/answerphone/Application.java
@@ -0,0 +1,26 @@
+package com.dpudov.answerphone;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by DPudov on 31.01.2016.
+ */
+public class Application extends android.app.Application {
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+
+// VKAccessToken is invalid
+            }
+        }
+    };
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+    }
+}
