fa0473c1f9e7addeab91e8c6658cac3a9f7a9b93
DPudov <dimapudov99@gmail.com>
2016-02-09 08:29:48 +0300

    Bug dies.. 09/02

--- src/main/java/com/dpudov/answerphone/fragments/MessagesService.java
+++ src/main/java/com/dpudov/answerphone/fragments/MessagesService.java
@@ -106,7 +106,7 @@ public class MessagesService extends Service {
     }
 
     private int[] getMsg() {
-        VKRequest request = VKApi.messages().get(VKParameters.from("out", 0, "time_offset", 600));
+        VKRequest request = VKApi.messages().get(VKParameters.from("time_offset", 360000));
         request.executeWithListener(new VKRequest.VKRequestListener() {
             @Override
             public void onComplete(VKResponse response) {
