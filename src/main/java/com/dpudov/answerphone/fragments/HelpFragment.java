2f7d7758c2c312c451ee44eb96bfb95206c915c4
DPudov <dimapudov99@gmail.com>
2016-02-15 16:46:42 +0300

    Продолжение разработки 15/02

--- src/main/java/com/dpudov/answerphone/fragments/HelpFragment.java
+++ src/main/java/com/dpudov/answerphone/fragments/HelpFragment.java
@@ -1,6 +1,5 @@
 package com.dpudov.answerphone.fragments;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -77,16 +76,6 @@ public class HelpFragment extends android.support.v4.app.Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
     @Override
     public void onDetach() {
